28e13706cb024637fae5fb8d23aeae230c6eabb8
Bohdan Shaban <bohdanshaban97@gmail.com>
2024-08-04 16:01:23 +0200

    AppInfo & SearchPanel Components Created...

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting Of Employees in Corporation ...</h1>
+      <h2>Total number of employees: {5} </h2>
+      <h2>The prize will be received by: </h2>
+    </div>
+  )
+}
+export default AppInfo;
